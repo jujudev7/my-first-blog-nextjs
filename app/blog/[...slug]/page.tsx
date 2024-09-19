6e8e5f87bca704a12e7e2e1d5423fe8ebb2ff88d
jujudev7 <penna.julien@gmail.com>
2024-09-19 16:55:31 +0200

    14. SEO and Favicon

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,6 +1,9 @@
 import { posts } from "#site/content";
 import { MDXContent } from "@/components/mdx-components";
+import { siteConfig } from "@/config/site";
 import "@/styles/mdx.css";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 interface PostPageProps {
   params: {
@@ -13,6 +16,45 @@ async function getPostFromParams(params: PostPageProps["params"]) {
   const post = posts.find((post) => post.slugAsParams === slug);
 
   return post;
+}
+
+export async function generateMetadata({
+  params,
+}: PostPageProps): Promise<Metadata> {
+  const post = await getPostFromParams(params);
+
+  if (!post) {
+    return {};
+  }
+
+  const ogSearchParams = new URLSearchParams();
+  ogSearchParams.set("title", post.title);
+
+  return {
+    title: posts.title,
+    description: posts.description,
+    authors: { name: siteConfig.author },
+    openGraph: {
+      title: posts.title,
+      description: posts.description,
+      type: "article",
+      url: posts.slug,
+      images: [
+        {
+          url: `/api/og?${ogSearchParams.toString()}`,
+          width: 1200,
+          height: 630,
+          alt: posts.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: posts.title,
+      description: posts.description,
+      images: [`/api/og?${ogSearchParams.toString()}`],
+    },
+  };
 }
 
 export async function generateStaticParams(): Promise<
